985d2f29e895e51c02ba9af339d05733919ad021
Shubh <shubh.scb@gmail.com>
2022-04-29 23:11:05 +0530

    GET ITEM SUCCESS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import './style.css';
-// import setItemToLocalStorage from './modules/setLocalStorage.js';
-// import getItemFromLocalStorage from './modules/getItemFromLocalStorage.js';
+import setItemToLocalStorage from './modules/setLocalStorage.js';
+import getItemFromLocalStorage from './modules/getItemFromLocalStorage.js';
 // import Icon from './refresh.svg';
 
 // const myIcon = new Image();
@@ -10,30 +10,24 @@ import './style.css';
 // const headLine = document.querySelector('.headline');
 // headLine.appendChild(Icon);
 
-const dynamicList = document.querySelector('.dynamic-list');
+
 const inputFieldValue = document.querySelector('#input');
 const onlyForm = document.querySelector('.only-form');
 
-let taskArray = [
-     { description: 'Wash car',
-       completed: false,
-       index: Date.now()
-},
-{ description: 'Hair cut',
-    completed: false,
-    index: Date.now()
-},
-{ description: 'Buy Grocery',
-    completed: false,
-    index: Date.now()
-},
-];
+let taskArray = [];
+let itemArray = [];
 
 
 onlyForm.addEventListener('submit', (e) => {
-    taskArray.forEach((taskObj) => {
-    dynamicList.innerHTML += `<div class="inner-div"><input type="checkbox">${taskObj.description}</div>`
-    });     
     e.preventDefault();
-   onlyForm.reset();
+    let taskObj = {
+        description: inputFieldValue.value,
+        completed: false,
+        index: Date.now()
+    };
+    taskArray.push(taskObj);
+    setItemToLocalStorage(taskArray);
+    onlyForm.reset();
+    getItemFromLocalStorage(itemArray);
+    
 });
--- src/modules/getItemFromLocalStorage.js
+++ src/modules/getItemFromLocalStorage.js
@@ -1,6 +1,13 @@
-const getItemFromLocalStorage = () => {
-    localStorage.getItem(JSON.parse('task'));
-    console.log(task);
+const dynamicList = document.querySelector('.dynamic-list');
+
+const getItemFromLocalStorage = (array) => {
+    let dataFromLocalStorage = localStorage.getItem('task');
+    array.push(JSON.parse(dataFromLocalStorage));
+    array.forEach((item) => {
+        dynamicList.innerHTML += `<div class="inner-div"><input type="checkbox">${item.description}</div>`;
+    })
+
+
 }
 
 export default getItemFromLocalStorage;
